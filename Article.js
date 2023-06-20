85cbd8a81ace940c4ef16097c020e1ab124b4414
Penyo <penyoofficial@outlook.com>
2023-06-20 14:44:18 +0800

    A230620
    - 新增《Penyo Blog 2.0？1.5！》
    - 引入 ESM 设计。

--- Article.js
+++ Article.js
@@ -2,6 +2,8 @@ export default { HTMLToText, zipHTML }
 
 /**
  * 将 HTML 代码转换为 textContent。
+ * @param {String} h HTML 代码
+ * @return {String} 纯文本
 */
 function HTMLToText(h) {
     return new String(h).
@@ -12,6 +14,8 @@ function HTMLToText(h) {
 
 /**
  * 压缩 HTML 代码。
+ * @param {String} rh 原始 HTML 代码
+ * @return {String} 压缩后的 HTML 代码
 */
 function zipHTML(rh) {
     return new String(rh).
